65988c8319b9dbe92224b5aa73648caa4774eb94
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-02-05 00:33:43 +0100

    trmnl_preview: init at 0.6.0

---
+++ pkgs/by-name/tr/trmnl_preview/package.nix
@@ -0,0 +1,47 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  ruby,
+  bundlerEnv,
+  versionCheckHook,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "trmnl_preview";
+  version = "0.6.0";
+
+  src = fetchFromGitHub {
+    owner = "usetrmnl";
+    repo = "trmnlp";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-vem1hkX5wCZRarWyuC6nhgEw4C42XY899NwngbS7jWc=";
+  };
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  installPhase =
+    let
+      env = bundlerEnv {
+        name = "trmnl_preview";
+        gemdir = ./.;
+        gemset = ./gemset.nix;
+      };
+    in
+    ''
+      mkdir -p $out/bin $out/share/trmnl_preview
+      cp -r . $out/share/trmnl_preview
+
+      makeWrapper ${ruby}/bin/ruby $out/bin/trmnlp \
+        --add-flags "-I $out/share/trmnl_preview/lib $out/share/trmnl_preview/bin/trmnlp" \
+        --set GEM_PATH "${env}/${env.ruby.gemPath}"
+    '';
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "version";
+
+  meta.mainProgram = "trmnlp";
+})

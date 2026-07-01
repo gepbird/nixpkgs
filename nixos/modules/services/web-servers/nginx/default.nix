61bb04965792e17ca8a3aace048af15e76f4b895
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-07-01 21:35:18 +0200

    nixos/nginx: add build time syntax checking

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -187,7 +187,13 @@ let
   '';
 
   configFile =
-    (if cfg.validateConfigFile then pkgs.writers.writeNginxConfig else pkgs.writeText) "nginx.conf"
+    let
+      writeNginxConfig = pkgs.writers.writeNginxConfig {
+        nginxPackage = cfg.package;
+        inherit (cfg) validateSyntax;
+      };
+    in
+    (if cfg.validateConfigFile then writeNginxConfig else pkgs.writeText) "nginx.conf"
       ''
         ${cfg.prependConfig}
 
@@ -1370,6 +1376,11 @@ in
       validateConfigFile = lib.mkEnableOption "validating configuration with pkgs.writeNginxConfig" // {
         default = true;
       };
+
+      validateSyntax = mkEnableOption "validating the syntax of the configuration" // {
+        default = cfg.validateConfigFile;
+        defaultText = lib.literalExpression "config.services.nginx.validateConfigFile";
+      };
     };
   };
 
@@ -1512,6 +1523,13 @@ in
           assertion = cfg.resolver.ipv4 || cfg.resolver.ipv6;
           message = ''
             At least one of services.nginx.resolver.ipv4 and services.nginx.resolver.ipv6 must be true.
+          '';
+        }
+
+        {
+          assertion = cfg.validateSyntax -> cfg.validateConfigFile;
+          message = ''
+            Validating syntax depends on validating the config file.
           '';
         }
       ]
